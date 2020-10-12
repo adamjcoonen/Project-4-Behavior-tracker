2e9cc49b148803dabe6ca0121fe5ce470f186dab
Adam <adamjcoonen@gmail.com>
2020-10-12 14:11:08 -0400

    user auth finished

---
+++ config/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.SECRET;
+
+module.exports = function(req, res, next) {
+  // is the token being sent in three different ways
+  let token = req.get('Authorization') || req.query.token || req.body.token;
+  if (token) {
+    // Remove the 'Bearer ' if it was included in the token header
+    token = token.replace('Bearer ', '');
+    // token valid
+    jwt.verify(token, SECRET, function(err, decoded) {
+      if (err) {
+        next(err);
+      } else {
+        // It's a valid token, so add user to req
+        req.user = decoded.user;    
+        next();
+      }
+    });
+  } else {
+    next();
+  }
+};
