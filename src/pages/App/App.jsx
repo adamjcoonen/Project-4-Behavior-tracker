2f28c6f330499ffa3ad3ba76f19e45bb13ea1114
Adam <adamjcoonen@gmail.com>
2020-10-16 12:41:44 -0400

    rename

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 import * as classroomAPI from '../../utils/classroom-api';
-import StudentDetails from '../studentDetails';
+import StudentDetails from '../StudentDetails';
 import UserDisplay from '../UserDisplay';
 import ClassroomDetails from '../ClassroomDetails';
 import SignupPage from '../SignupPage/SignupPage';
